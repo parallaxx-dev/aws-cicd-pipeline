fecbbeda9f79386e3c5c229776b223560fbc4351
Aaditya Pyarla <aaditya.pyarla@gmail.com>
2026-09-12 00:26:42 +0530

    fix: Fix the start container script to pull the image from the correct repository

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull parallaxxdev/aws-cicd-piepline
+docker pull paralaxxdev/aws-cicd-piepline
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 parallaxxdev/aws-cicd-piepline
+docker run -d -p 5000:5000 paralaxxdev/aws-cicd-piepline   
