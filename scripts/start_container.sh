d37abda963652d2a39f3e66734be8f44dc2305fe
Aaditya Pyarla <aaditya.pyarla@gmail.com>
2026-09-12 00:41:50 +0530

    Fix: Fix the web server to run on port 80 for compliance with AWS Security Groups

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull paralaxxdev/aws-cicd-piepline
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 paralaxxdev/aws-cicd-piepline   
+docker run -d -p 80:5000 paralaxxdev/aws-cicd-piepline   
